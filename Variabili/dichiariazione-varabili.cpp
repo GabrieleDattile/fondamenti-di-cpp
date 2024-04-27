da70e6163631f3b6e6d149a181500c1b7f4b33e2
Gabry <137740496+GabrieleDattile@users.noreply.github.com>
2024-04-27 21:44:02 +0200

    Create dichiariazione-varabili.cpp

---
+++ Variabili/dichiariazione-varabili.cpp
@@ -0,0 +1,4 @@
+int numero;  // Dichiarazione di una variabile di tipo intero chiamata "numero"
+float pi_greco;  // Dichiarazione di una variabile di tipo virgola mobile chiamata "pi_greco"
+char lettera;  // Dichiarazione di una variabile di tipo carattere chiamata "lettera"
+bool vero_o_falso;  // Dichiarazione di una variabile booleana chiamata "vero_o_falso"
