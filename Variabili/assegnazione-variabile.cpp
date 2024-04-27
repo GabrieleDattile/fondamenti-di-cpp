d84a09b596ddb6b0a8e5999350ec3196ed2dd63f
Gabry <137740496+GabrieleDattile@users.noreply.github.com>
2024-04-27 21:48:32 +0200

    Create assegnazione-variabile.cpp

---
+++ Variabili/assegnazione-variabile.cpp
@@ -0,0 +1,4 @@
+int numero = 10;  // Dichiarazione e inizializzazione della variabile "numero" con il valore 10
+float pi_greco = 3.14;  // Dichiarazione e inizializzazione della variabile "pi_greco" con il valore 3.14
+char lettera = 'A';  // Dichiarazione e inizializzazione della variabile "lettera" con il carattere 'A'
+bool vero_o_falso = true;  // Dichiarazione e inizializzazione della variabile "vero_o_falso" con il valore true
