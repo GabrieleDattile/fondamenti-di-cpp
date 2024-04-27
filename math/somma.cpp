6b97a9ff936bd060ee8f5dd7842ac2902f3a9e99
Goxu <140966822+LoLgoxu@users.noreply.github.com>
2024-04-27 22:43:41 +0200

    Update somma.cpp

--- math/somma.cpp
+++ math/somma.cpp
@@ -1,4 +1,5 @@
 #include <iostream>  // Includiamo la libreria iostream per utilizzare std::cout
+//usare il comando "using namespace std;" cosi' che non vi e' piu' bisogno di scrivere std:: a ogni comando
 
 int main() {
     int a = 5;  // Dichiarazione e inizializzazione di "a" con il valore 5
@@ -6,7 +7,7 @@ int main() {
     int somma = a + b;  // Calcolo della somma di "a" e "b"
 
     // Stampiamo il valore della variabile "somma" sulla console
-    std::cout << somma; // Stampiamo la somma
+    std::cout << somma /*<< std::endl;*/ // Stampiamo la somma
     std::cout << std::endl; // Aggiungiamo un carattere di nuova riga per pulire il buffer del flusso e andare a capo
 
     return 0;
