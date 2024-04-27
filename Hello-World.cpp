438fa85367281233263f221bb97f4e5df54ce7a0
Gabry <137740496+GabrieleDattile@users.noreply.github.com>
2024-04-27 21:34:15 +0200

    Create Hello-World.cpp

---
+++ Hello-World.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  // Includiamo la libreria iostream, che ci permette di utilizzare input e output
+
+int main() {  // Definiamo la funzione principale del programma, chiamata "main"
+
+    // std::cout è un oggetto di output standard che ci permette di scrivere dati sulla console
+    // "<<" è l'operatore di inserimento, che viene utilizzato per inserire dati nella console
+    // "Hello, World!" è il messaggio che vogliamo stampare
+    // "\n" è il carattere di nuova riga che ci permette di andare a capo dopo aver stampato il messaggio
+    std::cout << "Hello, World!";
+
+    return 0;  // Ritorniamo 0 per indicare che il programma è stato eseguito correttamente
